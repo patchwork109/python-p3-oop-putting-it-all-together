23e9485bdb74f93e649f3bf04421bdeedf766ffa
Samantha Genevay <samanthagenevay@gmail.com>
2023-03-18 13:26:48 -0700

    Completed Lab

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    
+    def __init__(self, title, author="", page_count=0, genre=""):
+        self.title = title
+        self.author = author
+        self.page_count = page_count
+        self.genre = genre
+
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if (type(page_count) == int):
+            self._page_count = page_count
+            print(f"Setting page count to {page_count} for {self.title}")
+        else:
+            print ("page_count must be an integer")
+    
+    page_count = property(get_page_count, set_page_count)
+
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+
+huck_finn = Book("Huck Finn", "Mark Twain", 150)
+nineteen_eighty_four = Book("1984", "George Orwell", "")
+huck_finn.turn_page()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, color="", size=5, material="", condition=""):
+        self.brand = brand
+        self.color = color
+        self.size = size
+        self.material = material
+        self.condition = condition
+
+    def cobble(self, condition="new"):
+        self.condition = "New"
+        print(f"Your shoe is as good as {condition}!")
+
+    def get_size(self):
+        print("Retrieving size")
+        return self._size
+
+    def set_size(self, size):
+        if (type(size) == int):
+            self._size = size
+            print(f"Setting size to {size}.")
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
+
+adidas = Shoe("Adidas", "Red", 5, "Leather", "Used")
+nike = Shoe("Nike", "White", "", "Canvas", "Used")
